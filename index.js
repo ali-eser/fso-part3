c9137baf38e8d8700fb96a418cdba57782f72c32
ali-eser <115903331+ali-eser@users.noreply.github.com>
2023-09-11 03:09:00 +0300

    Finish exercise 3.7

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+// const logger = morgan('tiny')
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+
 
 let persons = [
     { 
@@ -37,6 +43,7 @@ app.get('/api/persons', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
     const id = parseInt(request.params.id)
     const entry = persons.find(person => person.id === id)
+
     if (entry) {
         response.json(entry)
     } else {
