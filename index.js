66df5d0392cd3ef1c15913c61320290d6447e68d
ali-eser <115903331+ali-eser@users.noreply.github.com>
2023-09-13 00:21:43 +0300

    Finish exercise 3.9

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
+
 
 const app = express()
 
 app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 let persons = [
     { 
