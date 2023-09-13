67b36d773418ea7633ff7a45cb3198105504ec8e
ali-eser <115903331+ali-eser@users.noreply.github.com>
2023-09-14 02:04:38 +0300

    Finish exercises 3.13 & 3.14

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const morgan = require('morgan')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -9,45 +12,21 @@ app.use(express.static('dist'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/', (request, response) => {
     response.send('Hello World')
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(result => {
+        // console.log(result)
+        response.json(result)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = parseInt(request.params.id)
-    const entry = persons.find(person => person.id === id)
-    if (entry) {
-        response.json(entry)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id).then(person => {
+        response.json(person)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -58,22 +37,28 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     const newPerson = request.body
-    const randInt = (max) => Math.floor(Math.random() * max)
-    morgan.token('body', request => JSON.stringify(request.body))
-    const doesExist = persons.find(person => person.name === newPerson.name)
-    if (doesExist) {
-        response.status(400).send({
-                error: "name must be unique"
-        })
-    }
+
+    // const doesExist = Person.find({ name: newPerson.name })
+    // if (doesExist) {
+    //     response.status(400).send({
+    //             error: "name must be unique"
+    //     })
+    // }
+
     if (!newPerson.name || !newPerson.number) {
         response.status(400).send({
             error: "name and number fields must not be empty"
         })
+    } else {
+        const person = new Person({
+            name: newPerson.name,
+            number: newPerson.number
+        })
+    
+        person.save().then(savedPerson => {
+            response.json(savedPerson)
+        })
     }
-    newPerson.id = randInt(999999999999999)
-    persons = persons.concat(newPerson)
-    response.json(persons)
 })
 
 app.get('/info', (request, response) => {
@@ -82,7 +67,7 @@ app.get('/info', (request, response) => {
                     <p>${currentTime}</p>`)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
